b4d66d439ac8b2a3212f00beb3698720d4c62e96
Taro <pattanapat92@gmail.com>
2026-01-13 16:19:10 +0700

    ฟไ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        path.join(process.cwd(), "src/**/*.{js,ts,jsx,tsx,mdx}"),
     ],
     theme: {
         extend: {},
