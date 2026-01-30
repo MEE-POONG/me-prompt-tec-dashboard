675ace459619009d70365e5fb949beacab1c6ce3
Taro <pattanapat92@gmail.com>
2026-01-30 17:15:51 +0700

    up

--- src/pages/api/cloudflare-image/direct-upload.ts
+++ src/pages/api/cloudflare-image/direct-upload.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
-import fetch from "node-fetch";
+
 
 /**
  * API สำหรับขอ Direct Upload URL จาก Cloudflare
